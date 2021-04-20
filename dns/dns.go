e2845fe813283346ef16f719e63a0681893f27ca
Sven Dowideit <sven.dowideit@csiro.au>
2021-04-21 09:11:47 +1000

    add DNS settings to resolve.conf when they're not already there
    Signed-off-by: Sven Dowideit <sven.dowideit@csiro.au>

--- dns/dns.go
+++ dns/dns.go
@@ -75,7 +75,7 @@ func DnsServer(l service.Logger) {
 }
 
 func EnsureResolveConfigured(logger service.Logger) error {
-
+	logger.Infof("EnsureResolveConfigured")
 	// method - interestingly, this doesn't cache it, so this might be excellent for dynamic label based answers
 	// start this dns server
 	// add lines to /etc/systemd/resolved.conf as per https://github.com/hashicorp/consul/issues/4155#issuecomment-394362651
@@ -102,23 +102,40 @@ func EnsureResolveConfigured(logger service.Logger) error {
 	// Big nasty assumption that resolved.conf only contains a [Resolve] section
 	dnsline := "DNS=127.0.0.98"
 	//domainline := "Domains=~" + hostname + ".ona.im,~host.ona.im"
-	domainline := "Domains=~ona.im"
+	domainline := "Domains=~" + hostname + ".ona.im"
 	resolvedConfChanged := false
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.HasPrefix(line, "DNS=") {
 			if strings.Compare(line, dnsline) != 0 {
 				resolvedConfChanged = true
+				logger.Warningf("Replacing /etc/systemd/resolve.conf line (%s) with (%s)\n", line, dnsline)
 			}
 			text = append(text, dnsline)
+			// use empty string as indicator that we've written it already
+			dnsline = ""
 		} else if strings.HasPrefix(line, "Domains=") {
 			if strings.Compare(line, domainline) != 0 {
 				resolvedConfChanged = true
+				logger.Warningf("Replacing /etc/systemd/resolve.conf line (%s) with (%s)\n", line, domainline)
 			}
 			text = append(text, domainline)
+			// use empty string as indicator that we've written it already
+			domainline = ""
 		} else {
 			text = append(text, line)
 		}
+	}
+
+	if dnsline != "" {
+		resolvedConfChanged = true
+		logger.Warningf("Adding to /etc/systemd/resolve.conf : (%s)\n", dnsline)
+		text = append(text, dnsline)
+	}
+	if domainline != "" {
+		resolvedConfChanged = true
+		logger.Warningf("Adding to /etc/systemd/resolve.conf : (%s)\n", domainline)
+		text = append(text, domainline)
 	}
 	text = append(text, "")
 
